7b46f1bcc6635c93359f2342c161cee69f9d2f1f
roncr <ronaldcr10@gmail.com>
2016-05-10 21:35:30 -0700

    Added example of application constants and run methods

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,8 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
+
+import appConstants from './config/app.contants';
+import appRun from './config/app.run';
 import Components from './components/components';
 import CommonComponents from './common/common';
 import AppComponent from './app.component';
@@ -9,9 +12,11 @@ angular.module('app', [
     Components.name,
     CommonComponents.name
 ])
-.config(($locationProvider) => {
-    'ngInject'; // Annotates code to use ngAnnotate
-    // @see: https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions#how-to-configure-your-server-to-work-with-html5mode
-    $locationProvider.html5Mode(true).hashPrefix('!');
-})
-.component('app', AppComponent);
+    .constant(appConstants)
+    .config(($locationProvider) => {
+        'ngInject'; // Annotates code to use ngAnnotate
+        // @see: https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions#how-to-configure-your-server-to-work-with-html5mode
+        $locationProvider.html5Mode(true).hashPrefix('!');
+    })
+    .run(appRun)
+    .component('app', AppComponent);
